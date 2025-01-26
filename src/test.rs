3403658b09a174179871c6fb71f0c5b98910e400
domo <enoemo55@gmail.com>
2025-01-26 13:46:40 +0200

    Handle invalid commands

--- src/test.rs
+++ src/test.rs
@@ -2,17 +2,17 @@
 use std::io::{self, Write};
 
 fn main() {
-    // اطبع محث shell مع مسافة
+    // أطبع رمز $ مع مسافة
     print!("$ ");
-    
-    // تأكد من إظهار المحث فوراً
+
+    // تأكد من إظهار الرمز فورا
     io::stdout()
         .flush()
         .expect("❌ فشل في إرسال البيانات إلى الخرج");
 
     // احفظ إدخال المستخدم في متغير
     let mut user_command = String::new();
-    
+
     // اقرأ الإدخال حتى ضغط Enter
     io::stdin()
         .read_line(&mut user_command)
