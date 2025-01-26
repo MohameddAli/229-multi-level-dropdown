3632ba7e27225f7e6a19760084a4a9ca72ef578b
domo <enoemo55@gmail.com>
2025-01-26 14:09:27 +0200

    invalid commands

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,23 @@ fn main() {
     print!("$ ");
     
     // تأكد من إظهار المحث فوراً
+
     io::stdout()
+
         .flush()
+
         .expect("❌ فشل في إرسال البيانات إلى الخرج");
 
     // احفظ إدخال المستخدم في متغير
+
     let mut user_command = String::new();
-    
+
     // اقرأ الإدخال حتى ضغط Enter
+
     io::stdin()
+
         .read_line(&mut user_command)
+
         .expect("❌ فشل في قراءة الإدخال");
 
     println!("{}: command not found", user_command.trim());
